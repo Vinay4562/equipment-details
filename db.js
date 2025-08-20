7b95d779f1c29a517ac135f0b3ec34fda4fa7650
Vinay Kumar <manikantivinaykumar@yahoo.com>
2025-08-20 21:03:25 +0530

    Updated project files

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-  const uri = process.env.MONGO_URI;
-  if (!uri) throw new Error('MONGO_URI missing');
+  const uri = process.env.MONGO_URI || process.env.MONGODB_URI;
+  if (!uri) throw new Error('MONGO_URI/MONGODB_URI missing');
   await mongoose.connect(uri, { dbName: 'shankarpally_substation' });
   console.log('✅ MongoDB connected');
 };
